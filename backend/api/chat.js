58d8f5dcef1db53d64f8f5962156e9a9e182d2b2
Ovyzz <ovy_43@yahoo.ro>
2023-04-18 13:09:19 +0300

    fix: move socket

--- backend/api/chat.js
+++ backend/api/chat.js
@@ -7,41 +7,6 @@ const {
   isConversationHistoryEmpty,
 } = require("../helper/helper");
 
-const http = require("http");
-const server = http.createServer();
-const io = require("socket.io")(server, {
-  cors: {
-    origin: [
-      process.env.REACT_APP_FRONTEND_BASE_URL,
-      "https://admin.socket.io",
-    ],
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
-});
-server.listen(3002, () => {
-  console.log(`Websockets server run on port 3002`);
-});
-
-io.on("connection", (socket) => {
-  socket.on("join-room", (chatId) => {
-    socket.join(chatId);
-  });
-
-  socket.on("send-message", async (message) => {
-    const query = {
-      text: "INSERT INTO messages(user_id, chat_id, line_text) VALUES ($1, $2, $3)",
-      values: [message.userId, message.chatId, message.lineText],
-    };
-    try {
-      const response = await pool.query(query);
-      socket.to(message.chatId).emit("send-message", message);
-    } catch (error) {
-      console.log(error);
-    }
-  });
-});
-
 router.post("/add-friend", verifyToken, async (req, res) => {
   const { friendId } = req.query;
   const userId = req.user.id;
---
+++ backend/socket.js
@@ -0,0 +1,35 @@
+const pool = require("./pool");
+const http = require("http");
+const server = http.createServer();
+const io = require("socket.io")(server, {
+  cors: {
+    origin: [
+      process.env.REACT_APP_FRONTEND_BASE_URL,
+      "https://admin.socket.io",
+    ],
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+server.listen(3002, () => {
+  console.log(`Websockets server run on port 3002`);
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (chatId) => {
+    socket.join(chatId);
+  });
+
+  socket.on("send-message", async (message) => {
+    const query = {
+      text: "INSERT INTO messages(user_id, chat_id, line_text) VALUES ($1, $2, $3)",
+      values: [message.userId, message.chatId, message.lineText],
+    };
+    try {
+      const response = await pool.query(query);
+      socket.to(message.chatId).emit("send-message", message);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+});
